7ea3f7c472d654ab9d027ed1336ed6c58562a652
jaeeun <rosa2070@naver.com>
2024-04-18 17:42:34 +0900

    First commit

---
+++ default.js
@@ -0,0 +1,8 @@
+// javascript source
+document.write('<table border="1">');
+document.write('<tr>');
+document.write('<td>1열</td>');
+document.write('<td>2열</td>');
+document.write('<td>3열</td>');
+document.write('</tr>');
+document.write('</table>'); 
